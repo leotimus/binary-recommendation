b513a562d512d5c701d5683fb7a816dae98b93db
Dai Ha <dai.ha@magnolia-cms.com>
2020-11-03 15:56:00 +0100

    Add data fetcher

--- src/models/NeuCF.py
+++ src/models/NeuCF.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from src.prepare.DataFetcher import downloadData
 
 tqdm.pandas()
 np.set_printoptions(5, )
@@ -17,7 +19,8 @@ from tensorflow.keras.models import Model
 
 
 def getData(rows=200000):
-  df = pd.read_csv('data/lastfm_play.csv', nrows=rows)
+  downloadData()
+  df = pd.read_csv('prepare/lastfm_play.csv', nrows=rows)
   return df
 
 
@@ -39,10 +42,12 @@ def buildNeuralCollaborativeFilteringModel(numUser, numItem, numFactor):
   inputs = [userId, itemId]
   model = Model(inputs, output, name='NCF')
 
+  model.compile(Adam(1e-3), loss=BinaryCrossentropy(), metrics=[BinaryAccuracy()])
+
   return model
 
 
-def bootstrapDataset(df, negRatio=3., batchSize=128):
+def bootstrapDataset(df, negRatio=3., batchSize=128, shuffle=True):
   posDf = df[['user_id', 'artist_id']].copy()
   negDf = df[['user_id', 'artist_id']].sample(frac=negRatio, replace=True).copy()
   negDf.artist_id = negDf.artist_id.sample(frac=1.).values
@@ -57,37 +62,57 @@ def bootstrapDataset(df, negRatio=3., batchSize=128):
   }
   Y = mergeDf.label.values
 
-  ds = (tf.data.Dataset
-        .from_tensor_slices((X, Y))
+  ds = (tf.data.Dataset.from_tensor_slices((X, Y))
         .batch(batchSize))
-
+  if shuffle:
+    ds = ds.shuffle(buffer_size=len(df))
   return ds
 
 
 def main():
-  playDf = getData(600000)
+  checkpointPath = "prepare/checkpoints/NeuCF"
+  # checkpointDir = os.path.dirname(checkpointPath)
+  isTraining = False
+
+  playDf = getData(1000000)
 
   numUser = playDf.user_id.max() + 1
   numItem = playDf.artist_id.max() + 1
   num_factor = 32
+  epochs = 10
+  batchSize = 1024 * 16
 
   model = buildNeuralCollaborativeFilteringModel(numUser, numItem, num_factor)
 
-  model.compile(Adam(1e-3), loss=BinaryCrossentropy(), metrics=[BinaryAccuracy()])
+  train, test = train_test_split(playDf, test_size=0.2)
+  train, val = train_test_split(train, test_size=0.2)
+  print(len(train), 'train examples')
+  print(len(val), 'validation examples')
+  print(len(test), 'test examples')
 
-  #### TRAIN ####
-  num_epoch = 10
-  batch_size = 1024 * 16
-  for i in range(num_epoch):
-    print(f"{i + 1}th epoch :")
-    dataset = bootstrapDataset(playDf, num_epoch, batch_size)
-    model.fit(dataset)
+  testDataset = bootstrapDataset(test, epochs, batchSize, False)
+  if isTraining:
+    # Create a callback that saves the model's weights
+    checkpointCallBack = tf.keras.callbacks.ModelCheckpoint(filepath=checkpointPath,
+                                                            save_weights_only=True,
+                                                            verbose=1)
 
-  model.predict()
+    trainDataset = bootstrapDataset(train, epochs, batchSize)
+    valDataset = bootstrapDataset(val, epochs, batchSize, False)
+    model.fit(trainDataset, validation_data=valDataset, epochs=epochs, callbacks=[checkpointCallBack])
 
-  #### RUN FROM CHECKPOINT ####
+    print("Evaluating trained model...")
+    loss, accuracy = model.evaluate(testDataset)
+    print("Accuracy", accuracy)
+    print("Predict a sample:")
 
+  else:
+    model.load_weights(checkpointPath)
 
+    print("Predict a sample:")
+    predictions = model.predict(testDataset) #TODO give prediction to one entity only?
+
+    print("predictions shape:", predictions.shape)
 
 
 if __name__ == "__main__":
---
+++ src/prepare/DataFetcher.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import logging, gdown, os
+
+logger = logging.getLogger("DataFetcher")
+logging.basicConfig()
+
+def downloadData(url='https://drive.google.com/uc?id=1Xjse6RzUI34yD1TB7HkTYRhp_uIrBfjf',
+                 destDir="prepare"):
+  # prepare destination
+  dataFile = 'lastfm_play.csv'
+  dest = Path(destDir)
+  dest.parent.mkdir(parents=True, exist_ok=True)
+
+  # download zip
+  path = Path(destDir, dataFile)
+  if not path.exists():
+    logger.info("downloading file: %s.", dataFile)
+    gdown.download(url)
+    os.rename(dataFile, path.absolute())

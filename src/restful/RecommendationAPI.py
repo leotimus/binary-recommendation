e4d3d856b08414108df72aeb78fccc80eaa7c2cf
Dai Ha <dai.ha@magnolia-cms.com>
2020-11-20 12:19:48 +0100

    Get API works

--- src/restful/RecommendationAPI.py
+++ src/restful/RecommendationAPI.py
@@ -1,7 +1,11 @@
 from flask_oauthlib.provider import OAuth2Provider
 from flask import Flask
 
+from src.models import RModel
+from src.models.NeuMFModel import NeuMFModel
+
 app = Flask(__name__)
+activeModel:RModel
 
 # TODO oauth2 for strong security
 # oauth = OAuth2Provider(app)
@@ -11,7 +15,15 @@ app.config["DEBUG"] = True
 @app.route('/api/recommendation/<userId>', methods=['GET'])
 def getProductRecommendationForUser(userId):
   # Call model & feed the recommendation here
-  return [1,2,3,4,5]
+  global activeModel
+  return activeModel.predictForUser(userId)
+
+@app.route('/api/users', methods=['GET'])
+def getUsers():
+  # Call model & feed the recommendation here
+  global activeModel
+  return activeModel.getPredictableUsers()
+
 
 @app.route('/api/models', methods=['GET'])
 def getSupportedModels():
@@ -23,6 +35,17 @@ def getSupportedModels():
 # model in [NeuFM, MF...]
 def operateOnModel(operation, model):
   # Call model & feed the recommendation here
+  global activeModel
+  if operation == 'active':
+    activeModel = NeuMFModel('NeuMFModel')
+
+  elif operation == 'train':
+
+    trainingModel = NeuMFModel('NeuMFModel')
+    trainingModel.train('data/sdata.csv', 10000, {})
+
+    activeModel = trainingModel #TODO remove
+
   return 'Triggered operation {} on {} model'.format(operation, model)
 
 app.run()

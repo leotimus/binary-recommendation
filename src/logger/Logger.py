aa3849c865b7edd25b831187019cd94111d94f14
Dai Ha <dai.ha@magnolia-cms.com>
2020-12-16 06:22:50 +0100

    Get logger in place

---
+++ src/logger/Logger.py
@@ -0,0 +1,63 @@
+import logging
+import sys
+from logging.handlers import RotatingFileHandler
+from pathlib import Path
+
+
+def getLogger(name, logPath='logs/main.log', console=False) -> logging.Logger:
+  """
+  Simple logging wrapper that returns logger
+  configured to log into file and console.
+
+  Args:
+      name (str): name of logger
+      logPath (str): path of log file
+      console (bool): whether to log on console
+
+  Returns:
+      logging.Logger: configured logger
+  """
+  name = Path(sys.argv[0]).name if name == "__main__" else name
+  logger = logging.getLogger(name)
+  logger.setLevel(logging.DEBUG)
+  formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+  # ensure that logging handlers are not duplicated
+  for handler in list(logger.handlers):
+    logger.removeHandler(handler)
+
+  # rotating file handler
+  if logPath:
+    Path(logPath).parent.mkdir(parents=True, exist_ok=True)
+    fh = RotatingFileHandler(logPath,
+                             maxBytes=10 * 2 ** 20,  # 10 MB
+                             backupCount=1)  # 1 backup
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+  # console handler
+  if console:
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+  # null handler
+  if not (logPath or console):
+    logger.addHandler(logging.NullHandler())
+
+  return logger
+
+
+def float_array_string(arr):
+  """
+  format array of floats to 4 decimal places
+
+  Args:
+      arr: array of floats
+
+  Returns:
+      formatted string
+  """
+  return "[" + ", ".join(["{:.4f}".format(el) for el in arr]) + "]"
--- src/restful/ApiApp.py
+++ src/restful/ApiApp.py
@@ -3,7 +3,9 @@ import os
 import time
 
 from flask import Flask
+from flask.logging import default_handler
 
+from src.logger import Logger
 from src.restful.RecommendationEndpoint import RecommendationEndpoint
 from src.restful.oauth2.AuthenticationEndpoint import AuthenticationEndpoint
 from src.restful.oauth2.Oauth2 import configOauth2
@@ -13,7 +15,10 @@ from src.restful.oauth2.OauthModel import db, User, OAuth2Client
 class ApiApp:
 
   def __init__(self):
+    self._logger = Logger.getLogger(__name__, logPath='logs/server.log', console=True)
     self._app: Flask = Flask(__name__)
+    self._app.logger.removeHandler(default_handler)
+    self._app.logger.addHandler(self._logger.handlers[1])
     self.app.config.from_json('../../config.json')
 
     oauthEndpoints = AuthenticationEndpoint().oauthEndpoints
@@ -35,13 +40,16 @@ class ApiApp:
       if not self.app.config.get('DEV_MODE'):
         return
 
+      self.logger.info("In dev mode, auto create authentication database.")
       db.create_all()
 
+      self.logger.info("In dev mode, create default admin.")
       admin = User(username=self.app.config.get('DEF_ADMIN'),
                    password=hashlib.md5(self.app.config.get('DEF_ADMIN_PASS').encode()).hexdigest(),
                    scope='manager')
       db.session.add(admin)
 
+      self.logger.info("In dev mode, create default client.")
       client_id = self.app.config.get('DEV_CLIENT_ID')
       client_id_issued_at = int(time.time())
       adminClient = OAuth2Client(client_id=client_id, client_id_issued_at=client_id_issued_at, user_id=admin.id)
@@ -59,6 +67,10 @@ class ApiApp:
       db.session.add(adminClient)
 
       db.session.commit()
+
+  @property
+  def logger(self) -> Logger:
+      return self._logger
 
   @property
   def app(self) -> Flask:
